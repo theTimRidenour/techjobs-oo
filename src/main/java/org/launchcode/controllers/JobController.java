78e2653635d607aea6aa9f4917006e4347db152d
the Tim Ridenour <theTimRidenour@gmail.com>
2018-04-15 17:32:57 -0500

    Finished TODO 1 and 2

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,5 +1,6 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Job;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
@@ -23,7 +24,9 @@ public class JobController {
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model, int id) {
 
-        // TODO #1 - get the Job with the given ID and pass it into the view
+        Job theJob = new Job();
+        theJob = jobData.findById(id);
+        model.addAttribute("job", theJob);
 
         return "job-detail";
     }
